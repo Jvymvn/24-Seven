8b66ce8fad9fde2d009e41890c27aed0dc41d9d0
jvymvn <jordon.carter@yahoo.com>
2019-11-25 23:31:59 -0500

    Added add to cart button to productList

--- src/shop/CartSummary.js
+++ src/shop/CartSummary.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from "rect-router-dom";
+import { Link } from "react-router-dom";
 
 export class CartSummary extends Component {
     getSummary = () => {
