cf1efcee364226bcb5d6208c68f71ddb5e12f9ac
jvymvn <jordon.carter@yahoo.com>
2019-11-26 23:19:43 -0500

    Finished pagination of product list

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -15,3 +15,11 @@ export const loadData = (dataType, params) => ({
         params
     }))
 });
+
+export const setPageSize = (newSize) => ({
+  type: ActionTypes.DATA_SET_PAGESIZE, payload: newSize
+});
+
+export const setSortProperty = (newProp) => ({
+  type: ActionTypes.DATA_SET_SORT_PROPERTY, payload: newProp
+});
