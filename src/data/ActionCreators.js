422e8d76d814e0ccd6092775749e841c8ed5fafa
jvymvn <jordon.carter@yahoo.com>
2019-11-27 20:23:30 -0500

    push check

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -1,25 +1,31 @@
-import { ActionTypes } from "./Types";
-// import { data as phData } from "./placeholderData";
+import { ActionTypes, DataTypes } from "./Types";
 import { RestDataSource } from "./rest/RestDataSource";
 
 const dataSource = new RestDataSource();
 
 export const loadData = (dataType, params) => ({
   type: ActionTypes.DATA_LOAD,
-  payload: dataSource
-    .getData(dataType, params)
-    .then(response => ({ 
-        dataType, 
-        data: response.data, 
-        total: Number(response.headers["x-total-count"]),
-        params
-    }))
+  payload: dataSource.getData(dataType, params).then(response => ({
+    dataType,
+    data: response.data,
+    total: Number(response.headers["x-total-count"]),
+    params
+  }))
 });
 
-export const setPageSize = (newSize) => ({
-  type: ActionTypes.DATA_SET_PAGESIZE, payload: newSize
+export const setPageSize = newSize => ({
+  type: ActionTypes.DATA_SET_PAGESIZE,
+  payload: newSize
 });
 
-export const setSortProperty = (newProp) => ({
-  type: ActionTypes.DATA_SET_SORT_PROPERTY, payload: newProp
+export const setSortProperty = newProp => ({
+  type: ActionTypes.DATA_SET_SORT_PROPERTY,
+  payload: newProp
+});
+
+export const placeOrder = (order) => ({
+  type: ActionTypes.DATA_STORE,
+  payload: dataSource.StoreData(DataTypes.ORDERS,order).then(response => ({
+    dataType: DataTypes.ORDERS, data: response.data
+  }))
 });
